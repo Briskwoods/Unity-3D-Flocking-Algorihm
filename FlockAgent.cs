7497c2c01035407fc10ea72363ed8262c1fac84b
Jeff Gichuki <julian.gichuki@gmail.com>
2021-08-30 10:23:27 +0300

    Created FlockAgent.cs
    This defines the basic functionality of the flock agents.

---
+++ FlockAgent.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class FlockAgent : MonoBehaviour
+{
+    Collider agentCollider;
+    public Collider AgentCollider { get { return agentCollider; } }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agentCollider = GetComponent<Collider>();
+    }
+
+    public void Move(Vector2 velocity)
+    {
+        transform.forward = velocity;
+        transform.position = (Vector3)velocity * Time.deltaTime;
+    }
+}
